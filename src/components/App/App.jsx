97ed93fb07700c2b1fab491d8b0520f37f84d199
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2024-02-13 19:48:51 +0100

    add base files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import { Navbar } from '../Navbar/Navbar';
 import './App.module.css';
-import MoviesPage from '/Programming/GoIT/REACT/goit-react-hw-05/src/pages/MoviesPage/MoviesPage';
-import HomePage from '/Programming/GoIT/REACT/goit-react-hw-05/src/pages/HomePage/HomePage';
-import NotFoundPage from '/Programming/GoIT/REACT/goit-react-hw-05/src/pages/NotFoundPage/NotFoundPage';
+import MoviesPage from '../../pages/MoviesPage/MoviesPage';
+import HomePage from '../../pages/HomePage/HomePage';
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
